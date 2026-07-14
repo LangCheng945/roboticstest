0fb15565b405c776c88f7cd004cc171da8fe30b4
連成 <102245081+LangCheng945@users.noreply.github.com>
2026-07-15 06:37:42 +0800

    Create supabase.ts

---
+++ src/lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = https://yrzkezgzhdtaclyxdcaj.supabase.co;
+const supabaseKey = sb_publishable_eHXK1OZxNcwyICxu_3YAcg_0Ay-eR8t;
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
